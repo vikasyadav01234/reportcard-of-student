2c53ea6fc78e0950532fa6f27fa08e4363a49363
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2023-07-29 09:25:18 +0530

    Add files via upload

---
+++ areatriangleclass.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class vikas{
+	float b,h,area;
+	public:
+		void input()
+		{
+			cout<<"Enter The Value of Base:=";
+			cin>>b;
+			cout<<"Enter The Value of Height:=";
+			cin>>h;
+		}
+		void proce()
+		{
+			area=0.5f*b*h;
+		}
+		void output()
+		{
+			cout<<"Area of The triangle:="<<area;
+		}
+};
+void bubblesort()
+{
+//Bubble sort
+{
+=======
+void quicksort()
+{
+//Quick Sort//
+}
+void meargesort()
+{
+//mearge
+}
+int main()
+{
+	vikas obj;
+	obj.input();
+	obj.proce();
+	obj.output();
+cout<<"Vikas Yadav";
+
+	return 0;
+}
